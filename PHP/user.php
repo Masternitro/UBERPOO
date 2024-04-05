ae2ee512d15fcafe5611d6730b9297981301fa4f
Fernando Gonzalez <fg796041@gmail.com>
2024-04-05 13:20:43 -0600

    Documentos

---
+++ PHP/user.php
@@ -0,0 +1,52 @@
+<?php
+
+class User extends Account {
+
+    public function _construct($name,
+
+    $document, $email, $password){
+
+        parent::__construct($name,
+
+        $document, $email, $password);
+
+        {
+
+     public function printDatauser() {
+
+        echo "User";
+
+        echo"<br>";
+
+        echo "Name: $this->name
+
+        Document:$this->document
+
+        E-mail:Sthis->email
+
+        Password:$this->password" ;
+
+        echo "<br>";
+
+     }
+
+}
+
+?><?php
+class User extends Account {
+    public function _construct($name,
+    $document, $email, $password){
+        parent::__construct($name,
+        $document, $email, $password);
+        {
+     public function printDatauser() {
+        echo "User";
+        echo"<br>";
+        echo "Name: $this->name
+        Document:$this->document
+        E-mail:Sthis->email
+        Password:$this->password" ;
+        echo "<br>";
+     }
+}
+?>
